6110a9fd428875a1f0b291e745b6c7e8f78bdba2
Carlos Eduardo <cacadudugerlop@gmail.com>
2026-02-05 19:56:29 -0300

    Add Main class to initialize MenuPrincipal

---
+++ src/workhub/Main.java
@@ -0,0 +1,10 @@
+package workhub;
+
+import workhub.fronteira.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.executar();
+    }
+}
